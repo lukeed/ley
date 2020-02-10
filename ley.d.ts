98e3d54196cc059ebf1c85599b62441d7f27ec08
Luke Edwards <luke.edwards05@gmail.com>
2020-02-09 21:34:42 -0800

    feat: update type defs

--- ley.d.ts
+++ ley.d.ts
@@ -1,1 +1,18 @@
-export default function<T>(val: T): T;
+export namespace Options {
+	export interface Base {
+		cwd?: string;
+		client?: string;
+		dir?: string;
+	}
+
+	export interface Up extends Base {
+		single?: boolean;
+	}
+
+	export interface Down extends Base {
+		all?: boolean;
+	}
+}
+
+export function up(opts?: Options.Up): Promise<void>;
+export function down(opts?: Options.Down): Promise<void>;

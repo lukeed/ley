17a58836a965db77b75387d96138624fac0389fe
Luke Edwards <luke.edwards05@gmail.com>
2020-09-23 12:26:41 -0700

    feat(types): define `Driver` class

--- ley.d.ts
+++ ley.d.ts
@@ -5,6 +5,7 @@ declare namespace Options {
 	declare interface Base {
 		cwd?: string;
 		client?: string;
+		driver?: string | Driver;
 		require?: string | string[];
 		config?: Config | Resolver;
 		dir?: string;
@@ -27,6 +28,43 @@ declare namespace Options {
 
 declare class MigrationError extends Error {
 	readonly migration: Record<'name'|'abs', string>;
+}
+
+declare namespace Migration {
+	type Method = 'up' | 'down';
+
+	type Existing = Pick<File, 'name'>;
+
+	interface File {
+		/** The absolute file path */
+		abs: string;
+		/** The relative file path from `opts.dir` */
+		name: string;
+	}
+}
+
+export declare class Driver<Client = unknown> {
+	/**
+	 * Create a new Client connection using supplied options/config.
+	 * @important Must return the `Client` for further usage.
+	 */
+	connect<Client>(config: Options.Config): Promise<Client> | Client;
+	/**
+	 * Create `migrations` table and query for existing/applied migrations.
+	 * @note You may `require('ley/lib/text')` for premade SQL-like queries.
+	 * @important Must return the `name` of any existing migrations.
+	 */
+	setup(client: Client): Promise<Migration.Existing[]>;
+	/**
+	 * Loop `files` and apply the target `method` action.
+	 * @note Whenever possible, *all* files should partake in the same transaction.
+	 */
+	loop(client: Client, files: Migration.File[], method: Migration.Method): Promise<void>;
+	/**
+	 * Gracefully terminate your client.
+	 * Runs after *all* migrations have been applied, or after an error is thrown.
+	 */
+	end(client: Client): Promise<void> | void;
 }
 
 export function up(opts?: Options.Up): Promise<string[]>;
